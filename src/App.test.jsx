c17758142e0c6657d3f03415f07fd4b3e1ca7205
Michelle Nygren <michelle.r.nygren@gmail.com>
2022-01-27 09:46:38 -0800

    fixing testing folder structure

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,36 +1,91 @@
-import { render, screen } from '@testing-library/react';
-import BrewCard from './BrewCard';
+import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
+import App from './App';
+import userEvent from '@testing-library/user-event';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
 
-const beer = {
-  id: '10-56-brewing-company-knox',
-  name: '10-56 Brewing Company',
-  brewery_type: 'micro',
-  street: '400 Brown Cir',
-  address_2: null,
-  address_3: null,
-  city: 'Knox',
-  state: 'Indiana',
-  county_province: null,
-  postal_code: '46534',
-  country: 'United States',
-  longitude: '-86.627954',
-  latitude: '41.289715',
-  phone: '6308165790',
-  website_url: null,
-  updated_at: '2021-10-23T02:24:55.243Z',
-  created_at: '2021-10-23T02:24:55.243Z',
-};
+// create mock data
+const mockResponse = [
+  {
+    id: '10-56-brewing-company-knox',
+    name: 'MOCK MOCK MOCK 10-56 Brewing Company',
+    brewery_type: 'micro',
+    street: '400 Brown Cir',
+    address_2: null,
+    address_3: null,
+    city: 'Knox',
+    state: 'Indiana',
+    county_province: null,
+    postal_code: '46534',
+    country: 'United States',
+    longitude: '-86.627954',
+    latitude: '41.289715',
+    phone: '6308165790',
+    website_url: null,
+    updated_at: '2021-10-23T02:24:55.243Z',
+    created_at: '2021-10-23T02:24:55.243Z',
+  },
+  {
+    id: '10-torr-distilling-and-brewing-reno',
+    name: '10 Torr Distilling and Brewing',
+    brewery_type: 'micro',
+    street: '490 Mill St',
+    address_2: null,
+    address_3: null,
+    city: 'Reno',
+    state: 'Nevada',
+    county_province: null,
+    postal_code: '89502',
+    country: 'United States',
+    longitude: '-119.7732015',
+    latitude: '39.5171702',
+    phone: '7755307014',
+    website_url: 'http://www.10torr.com',
+    updated_at: '2021-10-23T02:24:55.243Z',
+    created_at: '2021-10-23T02:24:55.243Z',
+  },
+];
 
-test('renders a brewCard', async () => {
-  render(<BrewCard breweries={[beer]} />);
+// set up server
+const server = setupServer(
+  rest.get('https://api.openbrewerydb.org/breweries', (req, res, ctx) => {
+    // const select = req.url.searchParams.get('micro');
+    return res(ctx.json([mockResponse]));
+  })
+);
+// listen for server start
+beforeAll(() => server.listen());
 
-  const brewName = await screen.findByRole('heading', { name: beer.name });
-  expect(brewName).toBeInTheDocument();
+// console.log(server);
+// close server when done
+afterAll(() => server.close());
 
-  const brewLocation = screen.getByText(`${beer.city}, ${beer.state}`);
-  expect(brewLocation).toBeInTheDocument();
+// TEST 1
+test.only('renders the header and control panel', async () => {
+  render(<App />);
 
-  const brewType = await screen.findByText(beer.brewery_type);
-  expect(brewType).toBeInTheDocument();
-  screen.debug();
+  //   await waitForElementToBeRemoved(await screen.findByText('Loading...'), { timeout: 5000 });
+
+  const header = await screen.findByRole('heading', { name: /breweries!/i });
+  expect(header).toBeInTheDocument();
+
+  const controls = await screen.findByRole('combobox');
+  expect(controls).toBeInTheDocument();
+});
+
+// TEST 2
+test('renders a list of breweries on page load with default type setting', async () => {
+  render(<App />);
+
+  const types = await screen.findAllByRole('listitem');
+  expect(types).toHaveLength(20);
+});
+
+// TEST 3
+test('we can filter breweries by_type from dropdown', async () => {
+  render(<App />);
+
+  const controls = await screen.findByRole('combobox');
+  userEvent.selectOptions(controls, [screen.getByText('all')]);
+  expect(screen.getByRole('option', { name: 'all' }).selected).toBe(true);
 });
--- src/components/BrewCard/BrewCard.test.jsx
+++ src/components/BrewCard/BrewCard.test.jsx
@@ -1,75 +1,36 @@
-// import { render, screen } from '@testing-library/react';
-// import { rest } from 'msw';
-// import { setupServer } from 'msw/node';
-// import BrewCard from './BrewCard';
+import { render, screen } from '@testing-library/react';
+import BrewCard from './BrewCard';
 
-// // create mock data
-// const mockResponse = [
-//   {
-//     id: '10-56-brewing-company-knox',
-//     name: 'MOCK MOCK MOCK 10-56 Brewing Company',
-//     brewery_type: 'micro',
-//     street: '400 Brown Cir',
-//     address_2: null,
-//     address_3: null,
-//     city: 'Knox',
-//     state: 'Indiana',
-//     county_province: null,
-//     postal_code: '46534',
-//     country: 'United States',
-//     longitude: '-86.627954',
-//     latitude: '41.289715',
-//     phone: '6308165790',
-//     website_url: null,
-//     updated_at: '2021-10-23T02:24:55.243Z',
-//     created_at: '2021-10-23T02:24:55.243Z',
-//   },
-//   {
-//     id: '10-torr-distilling-and-brewing-reno',
-//     name: '10 Torr Distilling and Brewing',
-//     brewery_type: 'micro',
-//     street: '490 Mill St',
-//     address_2: null,
-//     address_3: null,
-//     city: 'Reno',
-//     state: 'Nevada',
-//     county_province: null,
-//     postal_code: '89502',
-//     country: 'United States',
-//     longitude: '-119.7732015',
-//     latitude: '39.5171702',
-//     phone: '7755307014',
-//     website_url: 'http://www.10torr.com',
-//     updated_at: '2021-10-23T02:24:55.243Z',
-//     created_at: '2021-10-23T02:24:55.243Z',
-//   },
-// ];
+const beer = {
+  id: '10-56-brewing-company-knox',
+  name: '10-56 Brewing Company',
+  brewery_type: 'micro',
+  street: '400 Brown Cir',
+  address_2: null,
+  address_3: null,
+  city: 'Knox',
+  state: 'Indiana',
+  county_province: null,
+  postal_code: '46534',
+  country: 'United States',
+  longitude: '-86.627954',
+  latitude: '41.289715',
+  phone: '6308165790',
+  website_url: null,
+  updated_at: '2021-10-23T02:24:55.243Z',
+  created_at: '2021-10-23T02:24:55.243Z',
+};
 
-// // set up server
-// const server = setupServer(
-//   rest.get('https://api.openbrewerydb.org/breweries', (req, res, ctx) => {
-//     // const select = req.url.searchParams.get('micro');
-//     return res(ctx.json([mockResponse]));
-//   })
-// );
-// // listen for server start
-// beforeAll(() => server.listen());
+test('renders a brewCard', async () => {
+  render(<BrewCard breweries={[beer]} />);
 
-// // console.log(server);
-// // close server when done
-// afterAll(() => server.close());
+  const brewName = await screen.findByRole('heading', { name: beer.name });
+  expect(brewName).toBeInTheDocument();
 
-// // test
-// test('renders brewCard', async () => {
-//   render(<BrewCard breweries={[server]} />);
+  const brewLocation = screen.getByText(`${beer.city}, ${beer.state}`);
+  expect(brewLocation).toBeInTheDocument();
 
-//   const brewName = await screen.findByRole('heading', { name: server[0].name });
-//   expect(brewName).toBeInTheDocument();
-
-//   const brewLocation = screen.getByText(`${server[0].city}, ${server[0].state}`);
-//   expect(brewLocation).toBeInTheDocument();
-
-//   const brewType = await screen.findByText(server[0].brewery_type);
-//   expect(brewType).toBeInTheDocument();
-//   screen.debug();
-// });
+  const brewType = await screen.findByText(beer.brewery_type);
+  expect(brewType).toBeInTheDocument();
+  screen.debug();
+});
