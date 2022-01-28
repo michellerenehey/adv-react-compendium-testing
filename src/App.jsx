5e6fb13bad583313f4d021927ce3a9eb4f5f1695
Michelle Nygren <michelle.r.nygren@gmail.com>
2022-01-27 17:00:39 -0800

    started styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from 'react';
-
 import './App.css';
 import { fetchBrews } from './services/breweries';
 import BrewCard from './components/BrewCard/BrewCard';
 import Controls from './components/Controls/Controls';
+import background from './assets/pattern.png';
 
 function App() {
   const [breweries, setBreweries] = useState([]);
@@ -36,15 +36,21 @@ function App() {
   if (loading) return <h3>Loading...</h3>;
 
   return (
-    <div className="App">
-      <h1>BREWERIES!</h1>
+    <div className="App" style={{ backgroundImage: `url(${background})` }}>
+      <h1 className="brewery-header">FIND-A-BREWERY!</h1>
       <Controls type={type} setType={setType} />
-      <div>
-        <BrewCard breweries={breweries} />
+      <BrewCard breweries={breweries} />
+      <div className="pagination">
+        <p className="currentPage">Current Page: {currentPage}</p>
+        {currentPage !== 1 && (
+          <button className="pagination-button" onClick={handlePrevPage}>
+            Prev Page
+          </button>
+        )}
+        <button className="pagination-button" onClick={handleNextPage}>
+          Next Page
+        </button>
       </div>
-      <p>Current Page: {currentPage}</p>
-      {currentPage !== 1 && <button onClick={handlePrevPage}>Prev Page</button>}
-      <button onClick={handleNextPage}>Next Page</button>
     </div>
   );
 }
