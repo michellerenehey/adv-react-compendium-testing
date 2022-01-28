8243b751c686f4cfb4a1d7495c593788dae4a4f5
Michelle Nygren <michelle.r.nygren@gmail.com>
2022-01-27 17:49:12 -0800

    styled control dropdown

--- src/components/Controls/Controls.jsx
+++ src/components/Controls/Controls.jsx
@@ -2,20 +2,23 @@ import './Controls.css';
 
 export default function Controls({ type, setType }) {
   return (
-    <label>
-      <select value={type} onChange={(e) => setType(e.target.value)}>
-        <option>all</option>
-        <option>micro</option>
-        <option>nano</option>
-        <option>regional</option>
-        <option>brewpub</option>
-        <option>large</option>
-        <option>planning</option>
-        <option>bar</option>
-        <option>contract</option>
-        <option>proprietor</option>
-        <option>closed</option>
-      </select>
-    </label>
+    <div className="dropdown">
+      Filter by type:
+      <label>
+        <select className="dropdown-select" value={type} onChange={(e) => setType(e.target.value)}>
+          <option>all</option>
+          <option>micro</option>
+          <option>nano</option>
+          <option>regional</option>
+          <option>brewpub</option>
+          <option>large</option>
+          <option>planning</option>
+          <option>bar</option>
+          <option>contract</option>
+          <option>proprietor</option>
+          <option>closed</option>
+        </select>
+      </label>
+    </div>
   );
 }
